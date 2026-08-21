52066b4fc645a14e6a60df0de0eef64d0082194e
ibuysausage <bannansmooth68@proton.me>
2026-08-21 14:14:18 -0400

    add docker for build_aur_packages.sh

--- scripts/build_aur_packages.sh
+++ scripts/build_aur_packages.sh
@@ -1,10 +1,25 @@
-git clone https://aur.archlinux.org/systemd-chromiumos
-cd systemd-chromiumos
-makepkg -s
-cp *.pkg.tar.zst ..
-cd ..
-git clone https://aur.archlinux.org/yay
-cd yay
-makepkg -s
-cp *.pkg.tar.zst ..
-cd ..
+docker run --rm -it \
+  -v "$(pwd)":/build \
+  -w /build \
+  archlinux:latest \
+  bash -c '
+    pacman -Syu --noconfirm &&
+    pacman -Sy --noconfirm base-devel git sudo gnupg less curl &&
+    useradd -m builder &&
+    echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers &&
+    chown -R builder:builder /build &&
+    su builder
+    cd /build
+    gpg --keyserver keyserver.ubuntu.com --recv-keys 286BF7EFCD77241E
+    git clone https://aur.archlinux.org/systemd-chromiumos
+    cd systemd-chromiumos
+    rm -rf PKGBUILD
+    curl -LO https://github.com/ibuysausage/pkgbuild-terraos/releases/download/v1.0/PKGBUILD
+    makepkg -s --noconfirm
+    cp *.pkg.tar.zst ..
+    cd ..
+    git clone https://aur.archlinux.org/yay
+    cd yay
+    makepkgs -s
+    cp *.pkg.tar.zst ..
+  '
